9320fdaa8d8609f338f5644146632d702ef13b7d
Schmxdt <airtonschmidt2018@gmail.com>
2023-02-21 20:45:17 -0300

    finished

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,57 +1,64 @@
-import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, LOCALE_ID } from '@angular/core';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/template/header/header.component';
+
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FooterComponent } from './components/template/footer/footer.component';
-import { NavComponent } from './components/template/nav/nav.component'
-import { MatSidenavModule } from '@angular/material/sidenav'
-import { MatCardModule } from '@angular/material/card'
+import { NavComponent } from './components/template/nav/nav.component';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
-import { RedDirective } from './directives/red.directive';
-import { ForDirective } from './directives/for.directive';
-import { ProductCreateComponent } from './components/product/product-create/product-create.component'
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
 
-import { FormsModule } from '@angular/forms'
-import { MatFormFieldModule } from '@angular/material/form-field'
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { ProductReadComponent } from './components/product/product-read/product-read.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort'
+import { MatSortModule } from '@angular/material/sort';
+import { RedDirective } from './directives/red.directive';
 
-import localeUS from '@angular/common/locales/es-US';
-import { registerLocaleData } from "@angular/common";
+import localePt from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+import { ForDirective } from './directives/for.directive';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
 
-registerLocaleData(localeUS);
+registerLocaleData(localePt);
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    FooterComponent,
     NavComponent,
     HomeComponent,
-    ProductCreateComponent,
     ProductCrudComponent,
-    FooterComponent,
+    ProductCreateComponent,
+    ProductReadComponent,
     RedDirective,
     ForDirective,
-    ProductReadComponent
+    ProductUpdateComponent,
+    ProductDeleteComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatSidenavModule,
-    MatListModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
     MatCardModule,
     MatButtonModule,
     MatSnackBarModule,
@@ -61,11 +68,11 @@ registerLocaleData(localeUS);
     MatInputModule,
     MatTableModule,
     MatPaginatorModule,
-    MatSortModule
+    MatSortModule,
   ],
   providers: [{
     provide: LOCALE_ID,
-    useValue: 'es-US'
+    useValue: 'pt-BR'
   }],
   bootstrap: [AppComponent]
 })
